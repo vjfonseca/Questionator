f0dd7e8c97853bb638773907784f4e9a7ecdffc8
victor <victorjvaf2012@gmail.com>
2021-04-22 21:13:14 -0300

    correção route questionario

---
+++ app/Data/QuestionarioRepo.cs
@@ -0,0 +1,17 @@
+using app.Models;
+
+namespace app.Data
+{
+    public class QuestionarioRepo : IQuestionarioRepo
+    {
+        public Questionario Create(Questionario questionario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Questionario GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
